c43452ac8c7df66e8fd20e12b7c50d7c9424e082
joegturner <59180847+joegturner@users.noreply.github.com>
2020-03-28 14:53:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const mainRoutes = require('./routes');
-const errorHandlers = require('./routes/errorhandlers');
-var app = express();
+const app = express();
 
 /** Middleware **/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
-
-
-/** Routes **/
+/** Routing **/
 app.use('/', mainRoutes);
 
+// catch 404 error
 app.use(function(req, res, next) {
     const err = new Error('Page not Found');
     err.status = 404;
@@ -24,27 +22,9 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    // res.locals.error = err;
-    // res.status(err.status);
     console.log(`${err.status} ${err.message}`);
-    return res.send(`Sorry, there was an error: "${err.status} ${err.message}"`);
+    res.render('error', {error: err});
 });
-
-// app.use(errorHandlers.handle404);
-// app.use(errorHandlers.handleGlobalError);
-
-// app.use((req, res, next) => {
-//     const err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-
-// // error handler
-// app.use((err, req, res, next) => {
-//     res.locals.error = err;
-//     res.status(err.status);
-//     res.send("uh oh ");
-// });
 
 // set up dev server, 3000 = port number
 app.listen(3000, () => {
