0f2fde32e2d5139719aa67cf14370cf3cea0a408
joegturner <59180847+joegturner@users.noreply.github.com>
2020-03-28 14:55:51 -0400

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+
+/** jQuery animation on project cover photos **/
+const $links = $('.link');
+const resize_value = 10;
+$($links).on('mouseenter', function() {
+    $(this).animate({ width: `+=${resize_value}`, height: `+=${resize_value}` }, 'fast');
+});
+$($links).on('mouseleave', function() {
+    $(this).animate({ width: `-=${resize_value}`, height: `-=${resize_value}` }, 'slow');
+});
