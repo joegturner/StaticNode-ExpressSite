2aedfccf06094f12b28145fa70310fb70a48c8f2
joegturner <59180847+joegturner@users.noreply.github.com>
2020-03-28 14:56:19 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,26 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json');
 
-
+// home page route
 router.get('/', (req, res, next) => {
     res.render('index', {projects});
 })
 
+// about page route
 router.get('/about', (req, res) => {
-    console.log('About page');
     res.render('about');
 });
 
+// projects page route
 router.get('/:id', (req, res, next) => {
     const projId = req.params.id;
     const project = projects.find( ({id}) => id === +projId);
+    
     if (project) {
-        console.log('proj');
         res.render('project', {project});
     } else {
+        // catch an error if URL extends passed an ID
+        // i.e. localhost:3000/1/nopage
         const err = new Error('Page not found');
         err.status = 404;
         return next(err);
