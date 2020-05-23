9e5a954b0be83f1014a37966f6a59c58a39d4523
joegturner <59180847+joegturner@users.noreply.github.com>
2020-05-23 04:23:30 -0400

    updated app.listen

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use(function(err, req, res, next) {
 });
 
 // set up dev server, 3000 = port number
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000');
-});
+// app.listen(3000, () => {
+//     console.log('The application is running on localhost:3000');
+// });
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
